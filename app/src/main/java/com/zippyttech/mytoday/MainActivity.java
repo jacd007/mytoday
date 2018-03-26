0beb32922e9e8214b58fdb8e7466abc1fc2992a3
Jesus <jacd0107@gmail.com>
2018-03-26 15:56:06 -0400

    add sharedPreferences

--- app/src/main/java/com/zippyttech/mytoday/MainActivity.java
+++ app/src/main/java/com/zippyttech/mytoday/MainActivity.java
@@ -1,14 +1,19 @@
 package com.zippyttech.mytoday;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String SHARED_KEY ="shared_key";
+
+    private SharedPreferences settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+       settings = getSharedPreferences(SHARED_KEY,0);
     }
 }
